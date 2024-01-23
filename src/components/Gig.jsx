742d2fafb2ab66a81e9a8c58ca0736fceffe40ee
AmyMcCann <amy.mccann.34@gmail.com>
2024-01-23 15:00:30 +0000

    Added CSS for Gig Component

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,13 +1,14 @@
+import "./Gig.css"
 import abbaImage from "../assets/abba.jpeg";
 
 const Gig = () => {
     return (
-        <div id="gig">
-        <h3 class="Band">ABBA</h3>
-        <img src={abbaImage}></img>
+        <div id="gig" class="gig">
+        <h3 class="band">ABBA</h3>
+        <img src={abbaImage} class="image"></img>
         <p class="gig-description">Get ready for the ABBA reunion gig. One night only and not to be missed - listen to all their classics, and some new music too!</p>
-        <p>7pm - Friday 23rd February 2024</p>
-        <p>Birmingham</p>
+        <p class="date">7pm - Friday 23rd February 2024</p>
+        <p class="location">Birmingham</p>
         </div>
     );
 };
