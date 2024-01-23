1bac496d9f6ab6fa6622ac9368c0744f9e8b48de
Ed <edward.gemmill@gmail.com>
2024-01-23 16:08:30 +0000

    react 03 props complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
 import "./App.css";
 import Gig from "./components/Gig"
 
+import abbaImage from "./assets/abba.jpeg";
+
+const gig1 = {
+    band: 'ABBA',
+    image: abbaImage,
+    description: "Get ready for the ABBA reunion gig. One night only and not to be missed - listen to all their classics, and some new music too!",
+    date: '7pm - Friday 23rd February 2024',
+    location: 'Birmingham'
+}
+
+const gig2 = {
+  band: 'Queen',
+  image: "https://m.media-amazon.com/images/M/MV5BMjI4OTIwNDAxMF5BMl5BanBnXkFtZTgwOTkzOTAyODE@._V1_.jpg",
+  description: "All your head-banging favourites",
+  date: '7pm - Saturday 24th February 2024',
+  location: 'Newcastle'
+}
+
+const gig3 = {
+  band: 'Carly Rae Jepsen',
+  image: "https://www.euphoriazine.com/wp-content/uploads/2023/08/carly-rae-jepsen.jpg",
+  description: "Party for 10,000",
+  date: '6.30pm - Sunday 25th February 2024',
+  location: 'Glasgow'
+}
+
 function App() {
   return (
-    <>
-      <Gig />
-    </>
+    <div>
+      <Gig band = {gig1.band}
+      image = {gig1.image}
+      description = {gig1.description}
+      date = {gig1.date}
+      location = {gig1.location}
+      />
+      <Gig band = {gig2.band}
+      image = {gig2.image}
+      description = {gig2.description}
+      date = {gig2.date}
+      location = {gig2.location}
+      />
+      <Gig band = {gig3.band}
+      image = {gig3.image}
+      description = {gig3.description}
+      date = {gig3.date}
+      location = {gig3.location}
+      />
+    </div>
   );
 }
 
--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,14 +1,14 @@
 import "./Gig.css"
-import abbaImage from "../assets/abba.jpeg";
 
-const Gig = () => {
+
+const Gig = (props) => {
     return (
         <div id="gig" class="gig">
-        <h3 class="band">ABBA</h3>
-        <img src={abbaImage} class="image"></img>
-        <p class="gig-description">Get ready for the ABBA reunion gig. One night only and not to be missed - listen to all their classics, and some new music too!</p>
-        <p class="date">7pm - Friday 23rd February 2024</p>
-        <p class="location">Birmingham</p>
+        <h3 class="band">{props.band}</h3>
+        <img src={props.image} class="image"></img>
+        <p class="gig-description">{props.description}</p>
+        <p class="date">{props.date}</p>
+        <p class="location">{props.location}</p>
         </div>
     );
 };
