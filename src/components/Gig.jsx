e516427e7dd1fe0d30b841003c52dde7555928b0
AmyMcCann <amy.mccann.34@gmail.com>
2024-01-23 14:35:51 +0000

    Initial Project Setup

---
+++ src/components/Gig.jsx
@@ -0,0 +1,13 @@
+const Gig = () => {
+    return (
+        <div id="gig">
+        <h3 class="Band">ABBA</h3>
+        <img></img>
+        <p class="gig-description">Get ready for the ABBA reunion gig. One night only and not to be missed - listen to all their classics, and some new music too!</p>
+        <p>7pm - Friday 23rd February 2024</p>
+        <p>Birmingham</p>
+        </div>
+    );
+};
+
+export default Gig;
