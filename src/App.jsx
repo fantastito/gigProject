962a15038702b92477e29adf3e92a7b7bf0c3b0e
AmyMcCann <amy.mccann.34@gmail.com>
2024-01-23 14:48:20 +0000

    Completed the Gig component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Hello from "./components/Hello";
-import makersLogo from "./assets/Makers-Logo.png";
 import "./App.css";
+import Gig from "./components/Gig"
 
 function App() {
   return (
     <>
-      <Hello name="World" />
-      <img className="logo" src={makersLogo}></img>
+      <Gig />
     </>
   );
 }
--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,8 +1,10 @@
+import abbaImage from "../assets/abba.jpeg";
+
 const Gig = () => {
     return (
         <div id="gig">
         <h3 class="Band">ABBA</h3>
-        <img></img>
+        <img src={abbaImage}></img>
         <p class="gig-description">Get ready for the ABBA reunion gig. One night only and not to be missed - listen to all their classics, and some new music too!</p>
         <p>7pm - Friday 23rd February 2024</p>
         <p>Birmingham</p>
